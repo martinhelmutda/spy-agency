f9e94394c911e5c62ded9e2d805c1acdbcaf8670
Martin Helmut Dominguez Alvarez <martinhelmutmh@gmail.com>
2021-05-24 11:36:25 -0500

    PIP-Tool to CI

--- spy/spy/settings.py
+++ spy/spy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+from django.core.management.utils import get_random_secret_key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8i7o*_1%a!#0q9bhhu6^)p0--kn5=0+#jnm04b*j4!@xx79kcx'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
